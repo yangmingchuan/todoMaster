661c014235de90881e237d486bf6707f29b5b2f4
yangmingchuan <18768880074@163.com>
2018-09-27 10:58:19 +0800

    1.sprinner 样式暂时使用灰色字体

--- app/src/main/java/cn/white/ymc/todomaster/MainActivity.kt
+++ app/src/main/java/cn/white/ymc/todomaster/MainActivity.kt
@@ -3,6 +3,7 @@ package cn.white.ymc.todomaster
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 /**
  * 首页
@@ -12,20 +13,26 @@ import android.support.v7.app.AppCompatActivity
  */
 class MainActivity : AppCompatActivity() {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    }
+
+    /**
+     * 底部按钮点击事件
+     */
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_home -> {
+
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_dashboard -> {
+
                 return@OnNavigationItemSelectedListener true
             }
         }
         false
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
     }
 }
