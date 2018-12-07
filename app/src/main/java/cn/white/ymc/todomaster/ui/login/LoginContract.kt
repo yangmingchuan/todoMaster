6f4eef9c28fe603c7218dcf1d125c7beacd98fd7
yangmingchuan <18768880074@163.com>
2018-12-07 18:22:01 +0800

    1.login activity start

---
+++ app/src/main/java/cn/white/ymc/todomaster/ui/login/LoginContract.kt
@@ -0,0 +1,15 @@
+package cn.white.ymc.todomaster.ui.login
+
+/**
+ * @packageName: cn.white.ymc.todomaster.ui.login
+ * @fileName: LoginContract
+ * @date: 2018/12/7  11:24
+ * @author: ymc
+ * @QQ:745612618
+ */
+
+class LoginContract{
+
+    interface
+
+}
